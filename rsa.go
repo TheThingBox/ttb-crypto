df39f6a62ecab862a48e9693b06ee297d5ac80c5
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-04-16 15:54:55 +0200

    fix initialisation variable (yes, again)

--- rsa.go
+++ rsa.go
@@ -25,7 +25,7 @@ type rsaPublic interface {
 }
 
 func loadPublicKey(path string) (rsaPublic, error) {
-  var block pem.Block
+  var block *pem.Block = nil
   if fileExists(path) == false {
     block, _ = pem.Decode([]byte(path))
     if block == nil {
@@ -99,7 +99,7 @@ type rsaPrivate interface {
 }
 
 func loadPrivateKey(path string) (rsaPrivate, error) {
-  var block pem.Block
+  var block *pem.Block = nil
   if fileExists(path) == false {
     block, _ = pem.Decode([]byte(path))
     if block == nil {
